de879bd3778aa4638a29d35c4b9eb8a1753481b2
kai <40947038S@ntnu.edu.tw>
2024-03-15 21:09:41 +0800

    update main

--- exp.py
+++ exp.py
@@ -1,4 +1,5 @@
 import fire
+import numpy as np
 from typing import Union
 
 from utils import TFSProblem, Solution
@@ -11,16 +12,37 @@ def main(
     alpha: float = 0.99,
     stopcriterion: Union[float, int] = 1,
     temperature: Union[float, int] = 1000,
+    times: int = 20,
+    output_dir: str = "output",
     verbose: bool = False,
 ):
     p = TFSProblem(data_dir)
-
     sa = SimulatedAnnealing(epoch_len, alpha, stopcriterion)
 
-    solution, search_steps = sa.search(p, temperature, verbose=verbose)
+    best = np.inf
+    best_sol = None
+    worst = 0
+    worst_sol = None
+    record = []
 
-    print(f"\n\nFinal solution: {solution} after {search_steps} steps.")
-    print(f"The minimum makespain is {p.evaluate(solution.sol)}.")
+    for i in range(tims):
+        solution, search_steps = sa.search(p, temperature, verbose=verbose)
+        makespan = p.evaluate(solution.sol)
+        record.append(makespan)
+
+        print(f"\n\nFinal solution: {solution} after {search_steps} steps.")
+        print(f"The minimum makespain is {makespain}.")
+
+        if makespan < best:
+            best = makespain
+            best_sol = solution
+        elif makespan > worst:
+            worst = makespain
+            worst_sol = solution
+
+    print(f"\n\nBest solutin {best_sol} has makespan {best}")
+    print(f"Worst solutin {worst_sol} has makespan {worst}")
+    print(f"Average makespan {sum(record) / len(record)}")
 
 
 if __name__ == "__main__":
