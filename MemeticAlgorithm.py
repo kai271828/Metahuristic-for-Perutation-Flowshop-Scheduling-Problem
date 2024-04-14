3326be6505e21a6de140c9f609258f97a9ee5a36
kai <40947038S@ntnu.edu.tw>
2024-04-14 21:29:51 +0800

    beta

--- MemeticAlgorithm.py
+++ MemeticAlgorithm.py
@@ -1,118 +1,141 @@
 import numpy as np
 from tqdm.auto import tqdm
 
-from utils import Solution, Population
+from utils import Solution
 from SimulatedAnnealing import SimulatedAnnealing
 
 
 class MemeticAlgorithm:
     def __init__(
-        self, cross_prob=0.5, muta_prob=0.2, epoch_len=10, alpha=0.9, temperature=3500
+        self,
+        init_ls=None,
+        init_ls_ratio=0.2,
+        tournament_k=2,
+        offspring_m=2,
+        cross_prob=0.5,
+        mutate_prob=0.2,
+        end_ls=None,
+        end_ls_ratio=0.2,
     ):
+        self.tournament_k = tournament_k
         self.cross_prob = cross_prob
-        self.muta_prob = muta_prob
-        self.epoch_len = epoch_len
-        self.alpha = alpha
-        self.temperature = temperature
+        self.mutate_prob = mutate_prob
+        self.init_ls_ratio = init_ls_ratio
+        assert (
+            init_ls is not None
+        ), "You have to assign an init_ls object with 'search(problem, init_sol=...)' method"
+        self.init_ls = init_ls
+        self.end_ls_ratio = end_ls_ratio
+        assert (
+            end_ls is not None
+        ), "You have to assign an end_ls object with 'search(problem, init_sol=...)' method"
+        self.end_ls = end_ls
 
-    def search(self, problem, p_size=20, t=10):
-        self.problem = problem
+    def search(self, problem, p_size=20, num_iter=10):
 
         # Initialization
-        pop = Population(length=problem.sol_length, size=p_size)
-        sa = SimulatedAnnealing(self.epoch_len, self.alpha, 1)
+        pop = Population(problem=problem, size=p_size)
+
+        # mutate offspring of local search of some initial solution
+        mutate_indices = np.random.choice(
+            pop.size, int(pop.size * self.init_ls_ratio), replace=False
+        )
+
+        for index in mutate_indices:
+            sa_result, _, _, _ = init_ls.search(problem, init_sol=pop[index])
+
+            i, j, k = np.random.choice(problem.sol_length, 3, replace=False)
+            mutated = MASolution(init_sol=sa_result).swap_3_mutate(i, j, k)
+
+            pop[i] = mutated
 
         # Evaluation
+        pop.evaluate_and_sort()
 
-        for i in tqdm(range(t)):
-            # Mating selection
-            # tournament selection
-            children_list = []
-
+        for i in tqdm(range(num_iter)):
             # Reproduction
-            for j in range(n // 2):
+            for j in range(int(pop.size // 2 * offspring_m)):
+                # Mating selection (tournament selection)
+                parent_id_1 = pop.k_tournament(self.tournament_k)
+                parent_id_2 = pop.k_tournament(self.tournament_k)
+
                 # Crossover
                 # order/linear order/partially-mapped crossover/cycle crossover
-                random_indices = np.random.choice(n, 2, replace=False)
-                cross_rand = np.random.random()
+                offspring_1, offspring_2 = pop.crossover(parent_id_1, parent_id_2)
 
-                children = self.cross_over(
-                    pop[random_indices[0]], pop[random_indices[1]]
-                )
+                random_num = np.random.random()
+                if random_num < self.mutate_prob:
+                    a, b = np.random.choice(problem.sol_length, 2, replace=False)
+                    start, end = np.min(a, b), np.max(a, b)
+                    offspring_1.insertion_mutate(start, end)
+                pop.append(offspring_1)
 
-                # Mutation
-                # insert
-                for child in children:
-                    muta_rand = np.random.random()
-                    if muta_rand < self.muta_prob:
-                        a, b = np.random.choice(
-                            self.problem.sol_length, 2, replace=False
-                        )
+                random_num = np.random.random()
+                if random_num < self.mutate_prob:
+                    a, b = np.random.choice(problem.sol_length, 2, replace=False)
+                    start, end = np.min(a, b), np.max(a, b)
+                    offspring_2.insertion_mutate(start, end)
 
-                        child.set_sol(child.swap_neighborhood(a, b))
+                pop.append(offspring_2)
 
-                    children_list.append(child)
-
-            pop.extend(children_list)
             # Evaluation
+            pop.evaluate_and_sort()
 
             # Environmental selection
-            pop = self.select_survivors(pop, n)
+            pop.environmental_select()
 
             # Local search
             # move operator should not be the same as crossover and mutation
-            for j in range(n - 5, n):
-                solution, _, _, _ = sa.search(
-                    self.problem, self.temperature, init_sol=pop[j]
-                )
-                pop[j].set_sol(solution.sol)
+            for j in range(p_size - 5, p_size):
+                solution, _, _, record = end_ls.search(problem, init_sol=pop[j])
+                solution.makespan = record["per_ffe"][-1]
+                pop[j] = solution
 
         return pop
 
-    def select_survivors(self, pop, num):
-        temp = sorted(
-            [(self.problem.evaluate(element.sol), element) for element in pop],
-            key=lambda x: x[0],
-        )
-        return [element[-1] for element in temp[:num]]
 
-    def cross_over(self, sol_a, sol_b):
-        a = Solution(self.problem.sol_length)
-        a.set_sol(sol_a.sol)
-        b = Solution(self.problem.sol_length)
-        b.set_sol(sol_b.sol)
-        return a, b
+class MASolution(Solution):
+    def __init__(self, length=20, init_sol=None):
+        super().__init__(length=length, init_sol=init_sol)
+
+    def swap_3_mutate(self, i, j, k):
+        temp = self[i]
+        self[i] = self[j]
+        self[j] = self[k]
+        self[k] = temp
+
+    def insertion_mutate(self, start, end):
+        """Mutate the solution by insertion
+        start index sould less than end index"""
+        temp = self[start]
+        for i in range(start, end):
+            if i + 1 < len(self.sol):
+                self[i] = self[i + 1]
+        self[end] = temp
 
 
 class Population:
     def __init__(
         self,
-        problem,
         size=20,
-        k=5,
-        sa_ratio=0.2,
-        sa_temperature=5000,
-        sa_epoch_length=15,
-        sa_alpha=0.98,
-        sa_stopcriterion=1,
+        init_k=5,
     ):
-        # mutated offsprings from local search + best N from kN random solutions
+        # best N from kN random solutions
         self._pop = []
+        self._size = size
 
-        sa_num = int(size * sa_ratio)
-        temp = [Solution(length=problem.sol_length) for i in range(sa_num)]
-        sa = SimulatedAnnealing(sa_epoch_length, sa_alpha, sa_stopcriterion)
+        temp = [
+            MASolution(length=self._problem.sol_length)
+            for i in range(self._size * init_k)
+        ]
 
-        for p in temp:
-            sa_p, _, _, _ = sa.search(problem, temperature, init_sol=p)
-            self._pop.append(sa_p)
+        temp.sort(key=lambda x: self._problem.evaluate(x))
 
-        temp = [Solution(length=problem.sol_length) for i in range(size * k)]
-        temp.sort(key=lambda x: problem.evaluate(x))
+        self._pop = temp[: (self._size)]
 
-        self._pop.extend(temp[: (size - sa_num)])
-
-        assert len(self._pop) == size, "There are some bugs in your init of Population"
+        assert (
+            len(self._pop) == self._size
+        ), "There are some bugs in your init of Population"
 
     def __getitem__(self, key):
         return self._pop[key]
@@ -120,5 +143,32 @@ class Population:
     def __setitem__(self, key, value):
         self._pop[key] = value
 
-    def evaluate(self):
-        pass
+    @property
+    def size(self):
+        return self._size
+
+    def append(self, p):
+        self._pop.append(p)
+
+    def evaluate_and_sort(self, problem):
+        for p in self._pop:
+            if not hasattr(p, "makespan"):
+                p.makespan = problem.evaluate(p)
+
+        self._pop.sort(key=lambda x: x.makespan)
+
+    def k_tournament(self, k):
+        """Return a index of best p from k random solutions
+        The Object(self) should be sorted."""
+        random_indices = np.random.choice(self.size, k, replace=False)
+        return np.min(random_indices)
+
+    def crossover(self, parent_id_1, parent_id_2):
+        offspring_1 = MASolution(init_sol=self._pop[parent_id_1])
+        offspring_2 = MASolution(init_sol=self._pop[parent_id_2])
+
+        return offspring_1, offspring_2
+
+    def environmental_select(self):
+        """Select the best k=self.size solution to survive"""
+        self._pop = self._pop[: self.size]
