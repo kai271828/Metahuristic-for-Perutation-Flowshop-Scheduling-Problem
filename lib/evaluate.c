b707a745d2879459ec486bea408857504390f7d6
kai <40947038S@ntnu.edu.tw>
2024-04-14 16:23:12 +0800

    debug

--- lib/evaluate.c
+++ lib/evaluate.c
@@ -2,13 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-static inline int64_t max(int x, int y) {
+static inline int64_t max(int64_t x, int64_t y) {
     return x > y ? x : y;
 }
 
 int64_t evaluate(int64_t *sol, int64_t **mj_table, int64_t num_machines, int64_t num_jobs) {
     int64_t *machine_cache = (int64_t*)calloc(num_machines, sizeof(int64_t));
 
+    printf("Start evaluate.so");
     for (int i = 0; i < num_jobs; i++) {
         for (int machine_id = 0; machine_id < num_machines; machine_id++) {
             if (machine_id != 0) {
@@ -21,5 +22,7 @@ int64_t evaluate(int64_t *sol, int64_t **mj_table, int64_t num_machines, int64_t
 
     int64_t makespan = machine_cache[num_machines - 1];
     free(machine_cache);
+
+    printf("Finish evaluate.so");
     return makespan;
 }
