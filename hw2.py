1ab25bdd753727e1f59f56fabfd2fd20f21b1c5f
kai <40947038S@ntnu.edu.tw>
2024-04-15 16:33:03 +0800

    reset

--- hw2.py
+++ hw2.py
@@ -1,8 +1,6 @@
 import fire
 import numpy as np
 from typing import Union
-from multiprocessing import Pool
-
 
 from utils import PFSProblem
 from SimulatedAnnealing import SimulatedAnnealing
@@ -26,95 +24,60 @@ def main(
     end_sa_ratio: float = 0.2,
     num_iter: int = 10,
     times: int = 20,
-    processing: int = 2,
     verbose: bool = False,
 ):
+    p = PFSProblem(data_dir)
+    init_sa = SimulatedAnnealing(
+        init_sa_temperature, init_sa_epoch_len, init_sa_alpha, 1
+    )
+    end_sa = SimulatedAnnealing(end_sa_temperature, end_sa_epoch_len, end_sa_alpha, 1)
+    ma = MemeticAlgorithm(
+        p_size=population_size,
+        init_k=init_k,
+        init_ls=init_sa,
+        init_ls_ratio=init_sa_ratio,
+        tournament_k=tournament_k,
+        offspring_m=offspring_m,
+        mutate_prob=mutate_prob,
+        end_ls=end_sa,
+        end_ls_ratio=end_sa_ratio,
+    )
 
-    if processing == 1:
-        p = PFSProblem(data_dir)
-        init_sa = SimulatedAnnealing(
-            init_sa_temperature, init_sa_epoch_len, init_sa_alpha, 1
+    best = np.inf
+    best_sol = None
+    worst = 0
+    worst_sol = None
+    makespan_record = []
+    diversity_record = []
+
+    for i in range(times):
+
+        print(f"[Experiment {i + 1}]")
+        results, ma_makespan_record, ma_diversity_record = ma.search(
+            p, num_iter=num_iter, verbose=verbose
         )
-        end_sa = SimulatedAnnealing(
-            end_sa_temperature, end_sa_epoch_len, end_sa_alpha, 1
-        )
-        ma = MemeticAlgorithm(
-            p_size=population_size,
-            init_k=init_k,
-            init_ls=init_sa,
-            init_ls_ratio=init_sa_ratio,
-            tournament_k=tournament_k,
-            offspring_m=offspring_m,
-            mutate_prob=mutate_prob,
-            end_ls=end_sa,
-            end_ls_ratio=end_sa_ratio,
-        )
+        results.evaluate_and_sort(problem=p)
 
-        best = np.inf
-        best_sol = None
-        worst = 0
-        worst_sol = None
-        makespan_record = []
-        diversity_record = []
+        print(f"Best makespan evolution in this experiment: {ma_makespan_record}")
+        print(f"Diversity evolution in this experiment: {ma_diversity_record}")
 
-        for i in range(times):
+        diversity_record.append(results.diversity)
+        makespan_record.append(results[0].makespan)
+        if results[0].makespan < best:
+            best = results[0].makespan
+            best_sol = results[0]
+        if results[0].makespan > worst:
+            worst = results[0].makespan
+            worst_sol = results[0]
 
-            print(f"[Experiment {i + 1}]")
-            results, ma_makespan_record, ma_diversity_record = ma.search(
-                p, num_iter=num_iter, verbose=verbose
-            )
-            results.evaluate_and_sort(problem=p)
+    mean = np.mean(makespan_record)
+    std = np.std(makespan_record)
 
-            print(f"Best makespan evolution in this experiment: {ma_makespan_record}")
-            print(f"Diversity evolution in this experiment: {ma_diversity_record}")
-
-            diversity_record.append(results.diversity)
-            makespan_record.append(results[0].makespan)
-            if results[0].makespan < best:
-                best = results[0].makespan
-                best_sol = results[0]
-            if results[0].makespan > worst:
-                worst = results[0].makespan
-                worst_sol = results[0]
-
-        mean = np.mean(makespan_record)
-        std = np.std(makespan_record)
-
-        print(f"\n\nBest solutin {best_sol} has makespan {best}")
-        print(f"Worst solutin {worst_sol} has makespan {worst}")
-        print(f"Mean: {mean}, Std: {std}\n")
-        print(f"Makespan record: {makespan_record}")
-        print(f"Diversity record: {diversity_record}")
-
-    else:
-
-        def subprocess(index):
-
-            print(f"[Experiment {index + 1}]")
-            results, ma_makespan_record, ma_diversity_record = ma.search(
-                p, num_iter=num_iter
-            )
-            results.evaluate_and_sort(problem=p)
-
-            print(f"Best makespan evolution in this experiment: {ma_makespan_record}")
-            print(f"Diversity evolution in this experiment: {ma_diversity_record}")
-
-            return results[0].makespan, results[0]
-
-        with Pool(processing) as p:
-            mp_record = p.map(
-                subprocess,
-                [index for index in range(times)],
-            )
-
-        mp_record.sort()
-        mean = np.mean(mp_record)
-        std = np.std(mp_record)
-
-        print(f"\n\nBest solutin {mp_record[0][1]} has makespan {mp_record[0][0]}")
-        print(f"Worst solutin {mp_record[0][1]} has makespan {mp_record[0][1]}")
-        print(f"Mean: {mean}, Std: {std}\n")
-        print(f"Makespan record: {[pair[0] for pair in mp_record]}")
+    print(f"\n\nBest solutin {best_sol} has makespan {best}")
+    print(f"Worst solutin {worst_sol} has makespan {worst}")
+    print(f"Mean: {mean}, Std: {std}\n")
+    print(f"Makespan record: {makespan_record}")
+    print(f"Diversity record: {diversity_record}")
 
 
 if __name__ == "__main__":
